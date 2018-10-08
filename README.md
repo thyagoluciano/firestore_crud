8cf3079295d2d955cb20fdac6edc89809f69503f
thyagoluciano <thyagoluciano@gmail.com>
2018-10-08 11:21:32 -0300

    Flutter project

---
+++ README.md
@@ -0,0 +1,8 @@
+# crud_firestore
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ android/app/src/main/java/br/com/samples/firestore/crudfirestore/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.samples.firestore.crudfirestore;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
